0b4f6f0ca8464e02299616d7b1602c86fd84250d
deguchi0527 <ryodegu0527@gmail.com>
2021-06-11 15:48:17 +0900

    アクションを作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,20 @@
 class CardsController < ApplicationController
+  def new
+    @card = Card.new
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @card = Card.new(crad_params)
+    if @card.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def crad_params
+      params.require(:card).permit(:title, :memo).merge(list_id: params[:list_id])
+    end
 end
