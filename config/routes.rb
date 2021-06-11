56c6a409a8ca1d93d4942e072145982ba87b44ad
deguchi0527 <ryodegu0527@gmail.com>
2021-06-11 17:36:42 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
   resources :lists, only: [:new, :create, :edit, :update, :destroy] do
-    resources :cards, only: [:new, :create, :show]
+    resources :cards, only: [:new, :create, :show, :edit, :update]
   end
 end
